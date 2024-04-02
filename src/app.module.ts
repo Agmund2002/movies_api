0bb0d9fec71aacd75d17496c89aaf8b048275493
Andrii Malysh <andriiymalysh@gmail.com>
2024-04-02 22:11:43 +0300

    update prisma schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,10 @@
 import { Module } from '@nestjs/common'
-import { TypeOrmModule } from '@nestjs/typeorm'
 import { ConfigModule } from '@nestjs/config'
-import { MovieEntity } from './movies/movie.entity'
 import { MovieModule } from './movies/movie.module'
-import { UserEntity } from './users/user.entity'
 import { UserModule } from './users/user.module'
 import { FavoritesModule } from './favorites/favorites.module'
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: process.env.DB_TYPE as any,
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [MovieEntity, UserEntity],
-      synchronize: true
-    }),
-    MovieModule,
-    UserModule,
-    FavoritesModule
-  ]
+  imports: [ConfigModule.forRoot(), MovieModule, UserModule, FavoritesModule]
 })
 export class AppModule {}
--- src/movies/movie.module.ts
+++ src/movies/movie.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common'
 import { MovieController } from './movie.controller'
-import { TypeOrmModule } from '@nestjs/typeorm'
-import { MovieEntity } from './movie.entity'
 import { MovieService } from './movie.service'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MovieEntity])],
   controllers: [MovieController],
-  providers: [MovieService],
-  exports: [TypeOrmModule]
+  providers: [MovieService]
 })
 export class MovieModule {}
